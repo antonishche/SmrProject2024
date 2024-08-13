a66f9c8b3ad8b0422bb7baf5d780b63f6e5d7937
antonishche <antonalfer2008@gmail.com>
2024-08-13 18:27:20 +0300

    added contacts

--- src/Pages/Contacts/Contacts.jsx
+++ src/Pages/Contacts/Contacts.jsx
@@ -5,7 +5,22 @@ import TopPanel from '../../Components/TopPanel/TopPanel'
 export default function Contacts() {
   return (
     <div className="container">
-      <TopPanel link={'/menu'}/>
+      <TopPanel link={'/menu'} name={'Chefis'} />
+      <div className="info_box">
+        <div className="adres">
+          <p>Адрес г. Минск<br />Неминга 4</p>
+        </div>
+        <div className="contacts">
+          <p>Контакты<br />+375 29 567 34 56<br />+375 44 578 09 67</p>
+        </div>
+        <div className="time_table">
+          <p>Время работы <br />пн-чт 12:00-23:00<br /> пт-вс 12:00-00:00</p>
+        </div>
+        <div className="digital">
+          <img src="facebook.png" alt="" />
+          <img src="instagram.png" alt="" />
+        </div>
+      </div>
     </div>
   )
 }
